eff21041706940f8f458f2a06bd46c8b06ba1ac7
Aliaksandr Fedukovich <1632637@mail.ru>
2024-09-15 16:39:17 +0300

    feat: initial commit, add express, prettier, nodemon

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('hello world. I am express')
+})
+
+app.listen(3000, () => {
+    console.log('server has already started on port 3000')
+})
