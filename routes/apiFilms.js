0440801a9092a02a54a378413f9a04b717b3697d
Aliaksandr Fedukovich <1632637@mail.ru>
2024-09-16 12:14:03 +0300

    feat: add create new movie

--- routes/apiFilms.js
+++ routes/apiFilms.js
@@ -73,6 +73,46 @@ filmRoutes.get("/readall", (req, res) => {
   });
 });
 
+filmRoutes.post("/create", (req, res) => {
+  const { id, title, rating, year, budget, gross, poster, position } = req.body;
+  if (
+    !id ||
+    !title ||
+    !rating ||
+    !year ||
+    !budget ||
+    !gross ||
+    !poster ||
+    !position
+  ) {
+    res.status(500).send("request invalid");
+  } else {
+    const newMovie = {
+      id: id,
+      title: title,
+      rating: rating,
+      year: year,
+      budget: budget,
+      gross: gross,
+      poster: poster,
+      position: position,
+    };
+
+    fs.readFile(path.join(__dirname, "../movies.txt"), (err, data) => {
+      const movieArr = JSON.parse(data.toString());
+      const newArr =
+        id > movieArr.length
+          ? fixIdInArray([...movieArr, newMovie])
+          : fixIdInArray([
+              ...movieArr.slice(0, id - 1),
+              newMovie,
+              ...movieArr.slice(id - 1),
+            ]);
+      writeUpdateMovieArray(res, newArr, newMovie);
+    });
+  }
+});
+
 module.exports = {
   filmRoutes,
 };
