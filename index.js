8fbf8de8f935eaf197e1e5643aabe3ed7b82c93a
Aliaksandr Fedukovich <1632637@mail.ru>
2024-09-16 09:07:57 +0300

    feat: add request for get movies array

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
+const express = require("express");
+const { getMoviesArr } = require("./getMovies");
 
 const app = express();
 
-app.get('/', (req, res)=> {
-    res.send('hello world. I am express')
-})
+app.get("/", (req, res) => {
+  res.send("hello world. I am express");
+});
 
 app.listen(3000, () => {
-    console.log('server has already started on port 3000')
-})
+  console.log("server has already started on port 3000");
+  /*     getMoviesArr(); */
+});
